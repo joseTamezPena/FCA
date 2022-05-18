abe85e7f645c4dba4d13f413fad210622f712520
José Gerardo Tamez-Peña <50146167+joseTamezPena@users.noreply.github.com>
2022-05-18 10:43:05 -0500

    Update irisexample.R
    Added histogram

--- RMD/irisexample.R
+++ RMD/irisexample.R
@@ -74,3 +74,43 @@ plot(irisDecor[,featuresDecor],col=classcolor,main="Unsupervised FCA IRIS")
 
 featuresDecor <- colnames(irisDecorOutcome[,sapply(irisDecorOutcome,is,"numeric")])
 plot(irisDecorOutcome[,featuresDecor],col=classcolor,main="Supervised FCA IRIS")
+
+## Plotting the histograms of the features
+par(mfrow=c(1,3))
+h <- hist(iris$Sepal.Length,main="Raw: Sepal Lenght")
+h <- hist(irisDecor$De_Sepal.Length,main="Unsup_FCA: Sepal Lenght")
+h <- hist(irisDecorOutcome$De_Sepal.Length,main="Sup_FCA: Sepal Lenght")
+
+h <- hist(iris$Sepal.Width,main="Raw: Sepal Width")
+h <- hist(irisDecor$De_Sepal.Width,main="Unsup_FCA: Sepal Width")
+h <- hist(irisDecorOutcome$De_Sepal.Width,main="Sup_FCA: Sepal Width")
+
+h <- hist(iris$Petal.Length,main="Raw: Petal Length")
+h <- hist(irisDecor$Ba_Petal.Length,main="Unsup_FCA: Petal Length")
+h <- hist(irisDecorOutcome$De_Petal.Length,main="Sup_FCA: Petal Length")
+
+h <- hist(iris$Petal.Width,main="Raw: Petal Width")
+h <- hist(irisDecor$De_Petal.Width,main="Unsup_FCA: Petal Width")
+h <- hist(irisDecorOutcome$Ba_Petal.Width,main="Sup_FCA: Petal Width")
+
+par(mfrow=c(1,1))
+boxplot(cbind(Raw=iris$Sepal.Length,
+              Unsup_FCA=irisDecor$De_Sepal.Length,
+              Sup_FCA=irisDecorOutcome$De_Sepal.Length),
+       main="Sepal Length")
+
+boxplot(cbind(Raw=iris$Sepal.Width,
+              Unsup_FCA=irisDecor$De_Sepal.Width,
+              Sup_FCA=irisDecorOutcome$De_Sepal.Width),
+        main="Sepal Width")
+
+
+boxplot(cbind(Raw=iris$Petal.Length,
+              Unsup_FCA=irisDecor$Ba_Petal.Length,
+              Sup_FCA=irisDecorOutcome$De_Petal.Length),
+        main="Petal Length")
+
+boxplot(cbind(Raw=iris$Petal.Width,
+              Unsup_FCA=irisDecor$De_Petal.Width,
+              Sup_FCA=irisDecorOutcome$Ba_Petal.Width),
+        main="Petal Width")
